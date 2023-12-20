3b3ea620c1b56159b91e36bf66f5a0601670d885
Nathan Halioua <nathanhalioua@gmail.com>
2023-12-20 16:15:56 +0100

    Les decorators marchent

--- src/representation/ImageNode.java
+++ src/representation/ImageNode.java
@@ -1,21 +1,21 @@
 package representation;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import fonctionGeneral.ImagePath;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import javax.imageio.ImageIO;
 
-// Décorateur concret pour l'image
 public class ImageNode extends NodeDecorator {
     private ImagePath imagePath;
-    
 
     public ImageNode(Event decoratedNode, ImagePath imagePath) {
         super(decoratedNode);
@@ -30,21 +30,54 @@ public class ImageNode extends NodeDecorator {
     }
 
     private void showImage() {
-        // Logique pour afficher l'image
         try {
             BufferedImage img = ImageIO.read(new File(imagePath.getPath()));
-            ImageIcon icon = new ImageIcon(img);
-            JLabel label = new JLabel(icon);
-            JOptionPane.showMessageDialog(null, label);
+
+            // Crée une fenêtre JFrame pour afficher l'image
+            JFrame frame = new JFrame();
+            frame.setTitle("Image Display");
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+            // Crée un JLabel pour afficher l'image
+            JLabel label = new JLabel(new ImageIcon(img));
+
+            // Ajoute le JLabel à la fenêtre
+            frame.getContentPane().add(label, BorderLayout.CENTER);
+
+            // Ajuste manuellement la taille de la fenêtre
+            int width = img.getWidth();
+            int height = img.getHeight();
+            frame.setSize(width + 50, height + 50); // Ajustez ces valeurs selon vos besoins
+
+            // Rend la fenêtre modale (elle apparaît en premier plan)
+            frame.setAlwaysOnTop(true);
+            frame.setModalExclusionType(Dialog.ModalExclusionType.APPLICATION_EXCLUDE);
+
+            // Centre la fenêtre sur l'écran
+            frame.setLocationRelativeTo(null);
+
+            // Rend la fenêtre visible
+            frame.setVisible(true);
+
+            // Crée un timer pour fermer la fenêtre après 4 secondes
+            Timer timer = new Timer(4000, new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    frame.dispose(); // Ferme la fenêtre
+                }
+            });
+
+            // Démarre le timer
+            timer.setRepeats(false); // Ne se répète pas
+            timer.start();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-	@Override
-	public void setNextNodes(List<Event> asList) {
-		decoratedEvent.setNextNodes(asList);
-		
-	}
+    
+    @Override
+    public void setNextNodes(List<Event> asList) {
+        decoratedEvent.setNextNodes(asList);
+    }
 }
-
